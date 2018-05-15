3c052ffee284139f77f879ffb699bb3fc44d97a9
VeraGerritseDeltion <arev_97@live.nl>
2018-05-15 17:17:32 +0200

    buildingPlacing

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Vera/Scripts/BuildPlacement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPlacement : MonoBehaviour {
+
+    [Header("testVar")]
+    public GameObject testBuilding;
+    public LayerMask obstacleLayer;
+    public LayerMask groundLayer;
+    bool startedPlacing;
+    GameObject isPlacing;
+    Building placing;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P)|| startedPlacing)
+        {
+            PlaceBuilding();
+        }
+	}
+
+    void PlaceBuilding()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Vector3 mouse3DPos = new Vector3(0,0,0);
+        if (Physics.Raycast(ray, out hit,1000,groundLayer))
+        {
+            mouse3DPos = hit.point;
+        }
+        else
+        {
+            return;
+        }
+
+        if (!startedPlacing)
+        {
+            startedPlacing = true;
+            isPlacing = Instantiate(testBuilding, mouse3DPos, Quaternion.identity);
+            placing = isPlacing.GetComponent<Building>();
+            placing.obstacles = obstacleLayer;
+        }
+        if(isPlacing != null && startedPlacing)
+        {
+            isPlacing.transform.position = mouse3DPos;
+            if(Input.GetButtonDown("Fire1") && !placing.inOtherBuilding)
+            {
+                placing.Place();
+                isPlacing = null;
+                startedPlacing = false;
+                placing = null;
+            }
+        }
+    }
+}
---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Vera/Scripts/Building.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+    public bool isPlaced;
+    public Material myMat;
+    public bool inOtherBuilding;
+    public LayerMask obstacles;
+    Color normalColor;
+	void Start () {
+        myMat = GetComponent<Renderer>().material;
+        normalColor = myMat.color;
+	}
+	
+	void Update () {
+        if (!isPlaced)
+        {
+            CollisionStay();
+        }
+	}
+
+    public void Place()
+    {
+        myMat.color = normalColor;
+        gameObject.layer = 8;
+        isPlaced = true;
+    }
+
+    void CollisionStay()
+    {
+        Collider[] buildings = Physics.OverlapSphere(transform.position,1,obstacles);
+        if (buildings.Length != 0)
+        {
+            myMat.color = Color.red;
+            inOtherBuilding = true;
+        }
+        else
+        {
+            myMat.color = Color.green;
+            inOtherBuilding = false;
+        }
+    }
+}
