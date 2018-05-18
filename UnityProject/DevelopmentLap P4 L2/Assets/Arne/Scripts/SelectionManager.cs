46e68a38d6a40ca483115b37b0603440bc35cb3b
Avonexile <arnevanommen@outlook.com>
2018-05-18 13:21:37 +0200

    particles and postprocessing stack

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Arne/Scripts/SelectionManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour {
+
+	public GameObject currentSelected;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	public void CheckInput () 
+	{
+		//click and send raycast, check the raycast if the correct tag
+		//is hit, make 
+	}
+}
