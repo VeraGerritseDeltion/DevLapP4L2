3098bceff7f63eb95b391f06c5ec19f268a4c51b
Rief Haalboom <superrief@hotmail.com>
2018-05-20 21:06:11 +0200

    begin building stats / player stats

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/BuildingTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Building", menuName = "New Building/Building")]
+public class BuildingTemplate : ScriptableObject{
+
+    public string name;
+    public string description;
+
+    public Sprite buildingSprite;
+
+    [Header("Plus")]
+    public int plusWood;
+    public int plusStone;
+    public int plusMoney;
+    public int plusHappiness;
+    public int plusCo2;
+    public int plusWater;
+    public int plusEnergy;
+
+    [Header("Minus")]
+    public int minWood;
+    public int minStone;
+    public int minMoney;
+    public int minHappiness;
+    public int minCo2;
+    public int minWater;
+    public int minEnergy;
+
+}
---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/Buildings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buildings : MonoBehaviour {
+
+    public BuildingTemplate building;
+
+
+
+	void Start () {
+	}
+	
+
+	void Update () {
+		
+	}
+}
