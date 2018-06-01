769428cadecf8020c10b3be04a126206f7531d49
Avonexile <arnevanommen@outlook.com>
2018-06-01 16:17:00 +0200

    skybox, titlescreen particles and tweaking to titlescreen tree

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Arne/Scripts/TitleScreenCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenCamera : MonoBehaviour {
+
+	public float speed;
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+	}
+}
