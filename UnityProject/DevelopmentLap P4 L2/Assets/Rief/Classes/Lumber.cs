c4584082cf0c5439684b3fba41fac97242207e58
Rief Haalboom <superrief@hotmail.com>
2018-06-08 15:49:31 +0200

    Bomen omhakken en plaatsen

--- UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/Lumber.cs
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/Lumber.cs
@@ -6,20 +6,60 @@ public class Lumber : MonoBehaviour {
 
 	public int myRadius;
 	public List<Collider> treeList;
+	public List<Collider> usedList;
 	public LayerMask tree;
 
-	void Start () {
+	public bool cutting;
+
+	int rotateSpeed = 1;
+
+	void Start()
+	{
 		AddTrees();
 	}
-	void AddTrees(){
-		Collider[] trees = Physics.OverlapSphere(transform.position, myRadius, tree);
+	void AddTrees()
+	{
+		Collider[] trees = Physics.OverlapSphere(transform.position,myRadius,tree);
 		treeList = new List<Collider>(trees);
-
-
+		
+		StartCoroutine(CutTrees());
+		StartCoroutine(PlantTrees());
 	}
 
-	IEnumerator TreeTime(){
-		yield return new WaitForSeconds(Random.Range(2,5));
-		treeList.Remove(treeList[Random.Range(0, treeList.Count)]);
+	IEnumerator CutTrees()
+	{
+		if(treeList.Count >= 1 && cutting)
+		{
+			int random = Random.Range(0, treeList.Count);
+			int randomTransY = Random.Range(0,360);
+
+			treeList[random].GetComponent<Animation>().Play("Cutting");
+			yield return new WaitForSeconds(treeList[random].GetComponent<Animation>().clip.length);
+			treeList[random].GetComponent<Animation>().Stop();
+
+			treeList[random].transform.localScale = new Vector3 (0,0,0);
+			treeList[random].transform.eulerAngles = new Vector3(0,randomTransY,0);
+
+			usedList.Add(treeList[random]);
+			treeList.Remove(treeList[random]);
+		}
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine(CutTrees());
+	}
+
+	IEnumerator PlantTrees()
+	{
+		if(usedList.Count >= 1 && !cutting)
+		{
+			int random = Random.Range(0, usedList.Count);
+
+			usedList[random].GetComponent<Animation>().Play("Growing");
+			yield return new WaitForSeconds(usedList[random].GetComponent<Animation>().clip.length);
+
+			treeList.Add(usedList[random]);
+			usedList.Remove(usedList[random]);
+		}
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine(PlantTrees());
 	}
 }
