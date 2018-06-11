b3cbfb16e255d42ad5d3fe5bdd2f5a818ddcf449
Rief Haalboom <superrief@hotmail.com>
2018-06-11 16:14:27 +0200

    Cropfield done

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/CropField.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CropField : MonoBehaviour {
+
+
+
+//moet nog UI references hebben
+
+
+	public bool wheatBool;
+	public GameObject wheat;
+	public GameObject carrot;
+
+	GameObject spawned;
+
+	void Start()
+	{
+		Spawning();
+	}
+	public void Spawning()
+	{
+		if(wheatBool)
+		{
+			spawned = Instantiate(wheat, new Vector3(transform.position.x, transform.position.y-0.6f, transform.position.z), Quaternion.Euler(-90, transform.rotation.y, transform.rotation.z));
+			
+		} 
+		else
+		{
+			spawned = Instantiate(carrot, new Vector3(transform.position.x, transform.position.y-0.2f, transform.position.z), Quaternion.Euler(-90, transform.rotation.y, transform.rotation.z));
+			
+		}
+	}
+	public void Harvest(){
+
+		Destroy(spawned.gameObject);
+	}
+}
---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/CropScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CropScript : MonoBehaviour {
+
+
+	public float growSpeed;
+
+	public bool harvest;
+	void Start () {
+	}
+	
+	void Update () {
+		MoveUp();
+	}
+
+	public void MoveUp()
+	{
+		Vector3 targetLoc = new Vector3(transform.position.x, 1, transform.position.z);
+		transform.position = Vector3.MoveTowards(transform.position, targetLoc, growSpeed * Time.deltaTime);
+
+		if(transform.position == targetLoc)
+		{
+			harvest = true;
+		}
+	}
+}
