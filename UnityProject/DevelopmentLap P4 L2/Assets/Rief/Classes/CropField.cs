83e317db7dc822b8d630e9f538c4addd194f16d8
Rief Haalboom <superrief@hotmail.com>
2018-06-11 16:58:27 +0200

    cropfield automated

--- UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/CropField.cs
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/CropField.cs
@@ -6,7 +6,7 @@ public class CropField : MonoBehaviour {
 
 
 
-//moet nog UI references hebben
+//moet nog UI links hebben.
 
 
 	public bool wheatBool;
@@ -14,26 +14,65 @@ public class CropField : MonoBehaviour {
 	public GameObject carrot;
 
 	GameObject spawned;
+	bool isGrowing = false;
+	public float growSpeed;
+	public int harvestAmount;
+	public int moneyCost;
 
-	void Start()
+
+	void Update()
 	{
-		Spawning();
+		Growing();
 	}
+
 	public void Spawning()
 	{
-		if(wheatBool)
-		{
-			spawned = Instantiate(wheat, new Vector3(transform.position.x, transform.position.y-0.6f, transform.position.z), Quaternion.Euler(-90, transform.rotation.y, transform.rotation.z));
-			
-		} 
-		else
-		{
-			spawned = Instantiate(carrot, new Vector3(transform.position.x, transform.position.y-0.2f, transform.position.z), Quaternion.Euler(-90, transform.rotation.y, transform.rotation.z));
-			
+		isGrowing = false;
+		StatisticManager.instance.money -= moneyCost * harvestAmount;
+
+		if(harvestAmount > 0)
+		{	harvestAmount --;
+			if(isGrowing == false)
+			{
+				isGrowing = true;
+				if(wheatBool)
+				{
+					spawned = Instantiate(wheat, new Vector3(transform.position.x, transform.position.y-0.6f, transform.position.z), Quaternion.Euler(-90, transform.rotation.y, transform.rotation.z));
+				} 
+				else
+				{
+					spawned = Instantiate(carrot, new Vector3(transform.position.x, transform.position.y-0.2f, transform.position.z), Quaternion.Euler(-90, transform.rotation.y, transform.rotation.z));
+				}
+			}
 		}
 	}
-	public void Harvest(){
 
+	void Growing()
+	{		
+		if(spawned != null){
+			Vector3 targetLoc = new Vector3(spawned.transform.position.x, 1, spawned.transform.position.z);
+			spawned.transform.position = Vector3.MoveTowards(spawned.transform.position, targetLoc, growSpeed * Time.deltaTime);
+
+			if(spawned.transform.position == targetLoc)
+			{
+				Harvest();
+			}
+		}
+	}
+
+	public void Harvest()
+	{
 		Destroy(spawned.gameObject);
+		Spawning();
+	}
+
+	public void HarvestUp()
+	{
+		harvestAmount++;
+	}
+	
+	public void HarvestDown()
+	{
+		harvestAmount--;
 	}
 }
