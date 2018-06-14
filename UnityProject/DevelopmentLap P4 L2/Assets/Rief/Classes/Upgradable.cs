f5eeac79d25f82cbe54240fde702fcba65e39023
Rief Haalboom <superrieff@hotmail.com>
2018-06-14 10:59:48 +0200

    Upgrades done

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/Upgradable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgradable : MonoBehaviour {
+
+
+	public GameObject upgradeMesh;
+	void Start () {
+		Upgrade();
+	}
+	
+	void Update () {
+		
+	}
+
+	public void Upgrade()
+	{
+		Mesh myMesh = upgradeMesh.GetComponent<MeshFilter>().sharedMesh;
+		GetComponent<MeshFilter>().mesh = myMesh;
+	}
+}
