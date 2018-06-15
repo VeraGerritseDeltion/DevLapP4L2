43c33be37b71fe5fe0aa63e8e057f82bc2631529
Rief Haalboom <superrieff@hotmail.com>
2018-06-15 10:35:40 +0200

    Water stuffs gedaan :)

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Other Assets/HolyShift/Scripts/ChangeColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    public Color startColor;
+    public Color endColor;
+    public Color currColor;
+
+    void Start () {
+        ChangingColor();
+        
+    }
+	
+	void Update () {
+		
+	}
+    void ChangingColor(){
+        currColor = Color.Lerp(startColor, endColor, NatureManager.instance.uitstoot / 100);
+		GetComponent<Renderer>().material.SetColor("_BaseColor", currColor);
+    }
+}
