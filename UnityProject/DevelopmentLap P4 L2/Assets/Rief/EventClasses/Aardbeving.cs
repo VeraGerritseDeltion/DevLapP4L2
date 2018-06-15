b5d6a661b286689ba8c18da49768e783b068f3f8
Rief Haalboom <superrieff@hotmail.com>
2018-06-15 16:35:28 +0200

    event dingen denk ik

--- UnityProject/DevelopmentLap P4 L2/Assets/Rief/EventClasses/Aardbeving.cs
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/EventClasses/Aardbeving.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class Aardbeving : Events{
 
-    int percentage;
+    public int percentage;
+    public int timer;
 
-   	public override void Occur()
+
+    public override void Occur()
     {
+        base.Occur();
+        GameManager.instance.myCamera.GetComponent<CameraShake>().shouldShake = true;
+        StartCoroutine(Happening());
+
+    }
+    IEnumerator Happening()
+    {
+        yield return new WaitForSeconds(timer);
+        int beenHit = 0;
         for (int i = 0; i < BuildingManager.instance.allBuildings.Count; i++)
         {
             if(Random.Range(0,100) < percentage){
                 //particle
-                BuildingManager.instance.allBuildings.Remove(BuildingManager.instance.allBuildings[i]);
+                //BuildingManager.instance.allBuildings.Remove(BuildingManager.instance.allBuildings[i]);
+                beenHit++;
             }
         }
+        GameManager.instance.myCamera.GetComponent<CameraShake>().shouldShake = false;
+        UIManager.instance.EventLog("The Earthquake destroyed " + beenHit + " Buildings!");
     }
 }
