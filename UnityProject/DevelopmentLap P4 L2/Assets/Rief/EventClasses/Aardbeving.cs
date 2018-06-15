c7a0c768babff311e6b0ba9ddddc287cf91bc638
Rief Haalboom <superrieff@hotmail.com>
2018-06-15 11:43:37 +0200

    event dingen weer

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/EventClasses/Aardbeving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aardbeving : Events{
+
+	
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+   	public override void Occur()
+    {
+
+    }
+}
--- UnityProject/DevelopmentLap P4 L2/Assets/Rief/EventClasses/Events.cs
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/EventClasses/Events.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class Events : MonoBehaviour {
 
+    public virtual void Occur()
+    {
 
+    }
 }
