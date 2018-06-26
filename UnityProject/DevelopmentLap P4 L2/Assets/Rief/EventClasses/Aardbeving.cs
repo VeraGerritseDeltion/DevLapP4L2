7edc397f4e809a498e1b6d1da3fb84bfa17d3c7c
VeraGerritseDeltion <arev_97@live.nl>
2018-06-26 09:21:04 +0200

    earthquake fiexed

--- UnityProject/DevelopmentLap P4 L2/Assets/Rief/EventClasses/Aardbeving.cs
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/EventClasses/Aardbeving.cs
@@ -6,7 +6,7 @@ public class Aardbeving : Events{
 
     public int percentage;
     public int timer;
-    public List<GameObject> hitBuildings;
+
 
     public override void Occur()
     {
@@ -19,22 +19,29 @@ public class Aardbeving : Events{
     IEnumerator Happening()
     {
         int beenHit = 0;
-
+        List<GameObject> hitBuildings = new List<GameObject>();
+        List<int> worksnow = new List<int>();
         yield return new WaitForSeconds(1);
         for (int i = 0; i < BuildingManager.instance.allBuildings.Count; i++)
         {
             if(Random.Range(0,100) < percentage){
                 BuildingManager.instance.allBuildings[i].GetComponent<Building>().dust.SetActive(true);
+                worksnow.Add(i);
                 hitBuildings.Add(BuildingManager.instance.allBuildings[i]);
             }
         }
 
         yield return new WaitForSeconds(timer);
 
-        for(int j = 0; j < hitBuildings.Count; j++){
-            BuildingManager.instance.allBuildings.Remove(BuildingManager.instance.allBuildings[j]);
-            BuildingManager.instance.allBuildings[j].GetComponent<Building>().EventDestroy();
+        for(int j = 0; j < worksnow.Count; j++)
+        {
+            BuildingManager.instance.allBuildings[worksnow[j]].GetComponent<Building>().EventDestroy();
+
             beenHit++;
+        }
+        for (int i = 0; i < hitBuildings.Count; i++)
+        {
+            BuildingManager.instance.allBuildings.Remove(hitBuildings[i]);
         }
         GameManager.instance.myCamera.GetComponent<CameraShake>().shouldShake = false;
         UIManager.instance.EventLog(beenHit.ToString() + " buildings destroyed.");
