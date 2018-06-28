8c61078763615b04ef0cff621813268bbdb234fa
Avonexile <arnevanommen@outlook.com>
2018-06-28 13:25:21 +0200

    building stuff

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Arne/Scripts/WindmillRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillRotate : MonoBehaviour {
+
+	float rot;
+	public float rotSpeed;
+
+	
+	void Update () 
+	{
+		if(Time.deltaTime != 0)
+		{
+			rot -= (1 * Time.deltaTime * rotSpeed);
+			gameObject.transform.localRotation = Quaternion.Euler(new Vector3(rot, 90 ,90));
+		}
+	}
+}
