ad15672c772947395ac38b8368c33ba3734e2330
Rief Haalboom <superrieff@hotmail.com>
2018-06-30 18:40:26 +0200

    Stockpile recourses simplified showing

---
+++ UnityProject/DevelopmentLap P4 L2/Assets/Rief/Classes/Stockpile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stockpile : MonoBehaviour {
+
+
+    public GameObject woodPile;
+    public GameObject stonePile;
+	
+	void Update () 
+	{
+        SetActive();
+    }
+	void SetActive()
+	{
+		if(StatisticManager.instance.wood > 0)
+		{
+            woodPile.SetActive(true);
+        }
+		else
+		{
+            woodPile.SetActive(false);
+        }
+
+		if(StatisticManager.instance.stone > 0)
+		{
+            stonePile.SetActive(true);
+        }
+		else
+		{
+            stonePile.SetActive(false);
+        }
+	}
+}
